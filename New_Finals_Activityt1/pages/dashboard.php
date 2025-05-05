5ba44dfed323e0b81d3fa81935f42fb2cacbf19f
Lord-binns <vince.olemberio2004@gmail.com>
2025-05-06 00:31:57 +0800

    added admin profile and css updates

--- New_Finals_Activityt1/pages/dashboard.php
+++ New_Finals_Activityt1/pages/dashboard.php
@@ -16,29 +16,36 @@
 </div>
 
 
-
-<!-- Sidebar -->
 <div id="sidebar" class="sidebar">
-    <a href="javascript:void(0)" class="closebtn" onclick="toggleSidebar()">×</a>
-    
+  <a href="javascript:void(0)" class="closebtn" onclick="toggleSidebar()">×</a>
+
+  <div class="sidebar-content">
     <div class="sidebar-links">
-        <a href="../pages/dashboard.php"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
-        <hr>
-        <a href="../pages/reports.php"><i class="fas fa-chart-bar"></i> Reports</a>
-        <hr>
-        <a href="../pages/inventory.php"><i class="fas fa-boxes"></i> Inventory</a>
-        <hr>
-        <a href="../pages/suppliers.php"><i class="fas fa-truck"></i> Suppliers</a>
-        <hr>
-        <a href="../pages/refunds_returns.php"><i class="fas fa-undo"></i> Refunds & Returns</a>
-        <hr>
-        <a href="../pages/user_log.php"><i class="fas fa-user-clock"></i> User Log</a>
+
+    <div class="admin-profile">
+    <img src="https://static.vecteezy.com/system/resources/previews/012/210/707/non_2x/worker-employee-businessman-avatar-profile-icon-vector.jpg" alt="Admin" class="admin-avatar">
+    <p class="admin-name">Admin</p>
+</div>
+<hr>
+      <a href="../pages/dashboard.php"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
+      <hr>
+      <a href="../pages/reports.php"><i class="fas fa-chart-bar"></i> Reports</a>
+      <hr>
+      <a href="../pages/inventory.php"><i class="fas fa-boxes"></i> Inventory</a>
+      <hr>
+      <a href="../pages/suppliers.php"><i class="fas fa-truck"></i> Suppliers</a>
+      <hr>
+      <a href="../pages/refunds_returns.php"><i class="fas fa-undo"></i> Refunds & Returns</a>
+      <hr>
+      <a href="../pages/user_log.php"><i class="fas fa-user-clock"></i> User Log</a>
     </div>
 
     <a href="../pages/logout.php" class="logout-link" onclick="confirmLogout(event)">
-        <i class="fas fa-power-off" style="color: red;"></i> Log out
+      <i class="fas fa-power-off" style="color: red;"></i> Log out
     </a>
+  </div>
 </div>
+
 
 
 <script>
