deea2671a051fd569ce382934782eee7328364af
Lord-binns <vince.olemberio2004@gmail.com>
2025-05-06 00:17:28 +0800

    updated sidebar design

--- New_Finals_Activityt1/pages/dashboard.php
+++ New_Finals_Activityt1/pages/dashboard.php
@@ -20,15 +20,26 @@
 <!-- Sidebar -->
 <div id="sidebar" class="sidebar">
     <a href="javascript:void(0)" class="closebtn" onclick="toggleSidebar()">×</a>
-    <a href="../pages/dashboard.php"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
-    <a href="../pages/inventory.php"><i class="fas fa-boxes"></i> Inventory</a>
-    <a href="../pages/suppliers.php"><i class="fas fa-truck"></i> Suppliers</a>
-    <a href="../pages/refunds_returns.php"><i class="fas fa-undo"></i> Refunds & Returns</a>
-    <a href="../pages/user_log.php"><i class="fas fa-user-clock"></i> User Log</a>
-    <a href="../pages/logout.php" onclick="confirmLogout(event)">
+    
+    <div class="sidebar-links">
+        <a href="../pages/dashboard.php"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
+        <hr>
+        <a href="../pages/reports.php"><i class="fas fa-chart-bar"></i> Reports</a>
+        <hr>
+        <a href="../pages/inventory.php"><i class="fas fa-boxes"></i> Inventory</a>
+        <hr>
+        <a href="../pages/suppliers.php"><i class="fas fa-truck"></i> Suppliers</a>
+        <hr>
+        <a href="../pages/refunds_returns.php"><i class="fas fa-undo"></i> Refunds & Returns</a>
+        <hr>
+        <a href="../pages/user_log.php"><i class="fas fa-user-clock"></i> User Log</a>
+    </div>
+
+    <a href="../pages/logout.php" class="logout-link" onclick="confirmLogout(event)">
         <i class="fas fa-power-off" style="color: red;"></i> Log out
     </a>
 </div>
+
 
 <script>
 function toggleSidebar() {
