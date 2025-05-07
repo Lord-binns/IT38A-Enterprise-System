d7ffcfc6648cb9a343cd2ec656c10b344f3c607b
Lord-binns <vince.olemberio2004@gmail.com>
2025-05-07 14:34:42 +0800

    added pashword hashing

--- New_Finals_Activityt1/pages/register.php
+++ New_Finals_Activityt1/pages/register.php
@@ -48,5 +48,22 @@
     <p>Already have an account? <a href="login.php">Login here</a></p>
 </div>
 
+<script>
+function togglePassword() {
+    const passwordInput = document.getElementById('password');
+    const eyeIcon = document.getElementById('eye-icon');
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        eyeIcon.classList.remove('fa-eye-slash');
+        eyeIcon.classList.add('fa-eye');
+    } else {
+        passwordInput.type = 'password';
+        eyeIcon.classList.remove('fa-eye');
+        eyeIcon.classList.add('fa-eye-slash');
+    }
+}
+</script>
+
+
 </body>
 </html>
