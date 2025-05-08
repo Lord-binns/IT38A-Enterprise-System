e897f04fd1c0792309f000003f3fdf1891f48da0
Lord-binns <vince.olemberio2004@gmail.com>
2025-05-08 13:27:02 +0800

    updated side bar route path of pages

--- New_Finals_Activityt1/admin_pages/dashboard.php
+++ New_Finals_Activityt1/admin_pages/dashboard.php
@@ -28,17 +28,17 @@
     <p class="admin-name">Admin Profile</p>
 </div>
 <hr>
-      <a href="../pages/dashboard.php"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
+      <a href="../admin_pages/dashboard.php"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
       <hr>
-      <a href="../pages/reports.php"><i class="fas fa-chart-bar"></i> Reports</a>
+      <a href="../admin_pages/reports.php"><i class="fas fa-chart-bar"></i> Reports</a>
       <hr>
-      <a href="../pages/inventory.php"><i class="fas fa-boxes"></i> Inventory</a>
+      <a href="../admin_pages/inventory.php"><i class="fas fa-boxes"></i> Inventory</a>
       <hr>
-      <a href="../pages/suppliers.php"><i class="fas fa-truck"></i> Suppliers</a>
+      <a href="../admin_pages/suppliers.php"><i class="fas fa-truck"></i> Suppliers</a>
       <hr>
-      <a href="../pages/refunds_returns.php"><i class="fas fa-undo"></i> Refunds & Returns</a>
+      <a href="../admin_pages/refunds_returns.php"><i class="fas fa-undo"></i> Refunds & Returns</a>
       <hr>
-      <a href="../pages/user_log.php"><i class="fas fa-user-clock"></i> User Log</a>
+      <a href="../admin_pages/user_log.php"><i class="fas fa-user-clock"></i> User Log</a>
     </div>
 
     <a href="javascript:void(0)" class="logout-link" onclick="showLogoutModal(event)">
